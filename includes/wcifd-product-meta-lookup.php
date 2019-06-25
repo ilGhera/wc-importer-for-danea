e0765a9fe9cc092442c2c372b105a02db8417d13
ilGhera <andrea@ilghera.com>
2019-06-25 22:58:20 +0200

    Aggiunto metodo di controllo presenza prodotto in tabella In fase di aggiornamento, se non presente il prodotto viene aggiunto

--- includes/wcifd-product-meta-lookup.php
+++ includes/wcifd-product-meta-lookup.php
@@ -40,19 +40,35 @@ class wcifdProductMetaLookup {
 	 */
 	private function setup_data( $data ) {
 
-		$this->product_id 	= isset( $data['product_id'] ) ? $data['product_id'] : '';
-		$this->sku 			= isset( $data['sku'] ) ? $data['sku'] : '';
-		$this->virtual 		= 0;
-		$this->downloadable 	= 0;
-		$this->min_price 		= isset( $data['min_price'] ) ? $data['min_price'] : '';
-		$this->max_price 		= isset( $data['max_price'] ) ? $data['max_price'] : '';
-		$this->onsale 		= isset( $data['onsale'] ) ? $data['onsale'] : '';
+		$this->product_id 	  = isset( $data['product_id'] ) ? $data['product_id'] : '';
+		$this->sku 			  = isset( $data['sku'] ) ? $data['sku'] : '';
+		$this->virtual 		  = 0;
+		$this->downloadable   = 0;
+		$this->max_price      = isset( $data['max_price'] ) ? $data['max_price'] : '';
+		$this->min_price      = isset( $data['min_price'] ) ? $data['min_price'] : '';
+		$this->onsale 		  = isset( $data['onsale'] ) ? $data['onsale'] : '';
 		$this->stock_quantity = isset( $data['stock_quantity'] ) ? $data['stock_quantity'] : '';
-		$this->stock_status 	= isset( $data['stock_status'] ) ? $data['stock_status'] : '';
-		$this->rating_count 	= 0;
+		$this->stock_status   = isset( $data['stock_status'] ) ? $data['stock_status'] : '';
+		$this->rating_count   = 0;
 		$this->average_rating = 0.00;
-		$this->total_sales 	= 0;
+		$this->total_sales 	  = 0;
 
+	}
+
+
+	/**
+	 * Verifica che il prodotto sia presente nella tabella wc_product_meta_lookup 
+	 * @return bool
+	 */
+	private function product_exists() {
+
+		global $wpdb;
+
+		$response = $wpdb->get_row( "SELECT * FROM " . $wpdb->prefix . "wc_product_meta_lookup WHERE product_id = " . $this->product_id );
+
+		$output = $response ? true : false;
+		
+		return $output;
 	}
 
 
@@ -63,39 +79,49 @@ class wcifdProductMetaLookup {
 
 		global $wpdb;
 
-		$output = $wpdb->insert(
+		if ( ! $this->product_exists() ) {
 
-			$wpdb->prefix . 'wc_product_meta_lookup',
-			array(
-				'product_id' 	 => $this->product_id,
-				'sku' 			 => $this->sku,
-				'virtual' 		 => $this->virtual,
-				'downloadable' 	 => $this->downloadable,
-				'min_price' 	 => $this->min_price,
-				'max_price' 	 => $this->max_price,
-				'onsale' 		 => $this->onsale,
-				'stock_quantity' => $this->stock_quantity,
-				'stock_status'   => $this->stock_status,
-				'rating_count' 	 => $this->rating_count,
-				'average_rating' => $this->average_rating,
-				'total_sales'	 => $this->total_sales,
-			),
-			array(
-				'%d',
-				'%s',
-				'%d',
-				'%d',
-				'%d',
-				'%d',
-				'%d',
-				'%d',
-				'%s',
-				'%d',
-				'%d',
-				'%d',
-			)
+			$output = $wpdb->insert(
 
-		);
+				$wpdb->prefix . 'wc_product_meta_lookup',
+				array(
+					'product_id' 	 => $this->product_id,
+					'sku' 			 => $this->sku,
+					'virtual' 		 => $this->virtual,
+					'downloadable' 	 => $this->downloadable,
+					'min_price' 	 => $this->min_price,
+					'max_price' 	 => $this->max_price,
+					'onsale' 		 => $this->onsale,
+					'stock_quantity' => $this->stock_quantity,
+					'stock_status'   => $this->stock_status,
+					'rating_count' 	 => $this->rating_count,
+					'average_rating' => $this->average_rating,
+					'total_sales'	 => $this->total_sales,
+				),
+				array(
+					'%d',
+					'%s',
+					'%d',
+					'%d',
+					'%d',
+					'%d',
+					'%d',
+					'%d',
+					'%s',
+					'%d',
+					'%d',
+					'%d',
+				)
+
+			);
+
+			// error_log( 'ADD: ' . print_r( $output, true ) );
+
+		} else {
+
+			$this->update();
+
+		}
 
 	}
 
@@ -107,32 +133,42 @@ class wcifdProductMetaLookup {
 
 		global $wpdb;
 
-		$output = $wpdb->update(
+		if ( $this->product_exists() ) {
 
-			$wpdb->prefix . 'wc_product_meta_lookup',
-			array(
-				'product_id' 	 => $this->product_id,
-				'sku' 			 => $this->sku,
-				'min_price' 	 => $this->min_price,
-				'max_price' 	 => $this->max_price,
-				'onsale' 		 => $this->onsale,
-				'stock_quantity' => $this->stock_quantity,
-				'stock_status'   => $this->stock_status,
-			),
-			array(
-				'product_id' => $this->product_id,
-			),
-			array(
-				'%d',
-				'%s',
-				'%d',
-				'%d',
-				'%d',
-				'%d',
-				'%s',
-			)
+			$output = $wpdb->update(
 
-		);
+				$wpdb->prefix . 'wc_product_meta_lookup',
+				array(
+					'product_id' 	 => $this->product_id,
+					'sku' 			 => $this->sku,
+					'min_price' 	 => $this->min_price,
+					'max_price' 	 => $this->max_price,
+					'onsale' 		 => $this->onsale,
+					'stock_quantity' => $this->stock_quantity,
+					'stock_status'   => $this->stock_status,
+				),
+				array(
+					'product_id' => $this->product_id,
+				),
+				array(
+					'%d',
+					'%s',
+					'%d',
+					'%d',
+					'%d',
+					'%d',
+					'%s',
+				)
+
+			);
+
+			// error_log( 'AGG: ' . print_r( $output, true ) );
+
+		} else {
+
+			$this->add();
+
+		}
 
 	}
 
@@ -154,6 +190,8 @@ class wcifdProductMetaLookup {
 				'%d',
 			)
 		);
+
+		// error_log( 'DEL: ' . print_r( $output, true ) );
 
 	}
 
