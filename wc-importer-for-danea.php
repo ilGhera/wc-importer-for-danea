0acfad81341aac9882ae2d590de1e2408f3883e8
ilGhera <andrea@ilghera.com>
2020-02-05 16:27:11 +0100

    Modifiche main file

--- wc-importer-for-danea.php
+++ wc-importer-for-danea.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name: WC Importer for Danea - Premium
- * Plugin URI: https://www.ilghera.com/product/woocommerce-importer-for-danea-premium/
- * Description: If you've built your online store with Woocommerce and you're using Danea Easyfatt as management software, you definitely need Woocommerce Importer for Danea - Premium!
+ * Plugin URI: https://www.ilghera.com/product/woocommerce-importer-for-danea/
+ * Description: If you've built your online store with Woocommerce and you're using Danea Easyfatt as management software, you definitely need Woocommerce Importer for Danea.
  * You'll be able to import suppliers, clients and products.
  * Author: ilGhera
- * Version: 1.2.1
+ * Version: 1.1.0
  * Author URI: https://ilghera.com
  * Requires at least: 4.0
  * Tested up to: 5.3
@@ -23,19 +23,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Attivazione plugin
  */
-function load_wc_importer_for_danea_premium() {
+function load_wc_importer_for_danea() {
 
 	/*Function check */
 	if ( ! function_exists( 'is_plugin_active' ) ) {
 		require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
-	}
-
-	/*Disattiva il plugin free se presente*/
-	if ( is_plugin_active( 'wc-importer-for-danea/wc-importer-for-danea.php' ) || function_exists( 'load_wc_importer_for_danea' ) ) {
-		deactivate_plugins( 'wc-importer-for-danea/wc-importer-for-danea.php' );
-		remove_action( 'plugins_loaded', 'load_wc_importer_for_danea' );
-		wp_redirect( admin_url( 'plugins.php?plugin_status=all&paged=1&s' ) );
-
 	}
 
 	/*Dichiarazioni costanti*/
@@ -62,23 +54,4 @@ function load_wc_importer_for_danea_premium() {
 	require( WCIFD_INCLUDES . 'wcifd-import-orders.php' );
 
 }
-add_action( 'plugins_loaded', 'load_wc_importer_for_danea_premium', 1 );
-
-
-/*Richiamo "Update-Checker"*/
-require( plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php' );
-$wcifdUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://www.ilghera.com/wp-update-server-2/?action=get_metadata&slug=wc-importer-for-danea-premium',
-	__FILE__,
-	'wc-importer-for-danea-premium'
-);
-
-$wcifdUpdateChecker->addQueryArgFilter( 'wcifd_secure_update_check' );
-function wcifd_secure_update_check( $queryArgs ) {
-	$key = base64_encode( get_option( 'wcifd-premium-key' ) );
-
-	if ( $key ) {
-		$queryArgs['premium-key'] = $key;
-	}
-	return $queryArgs;
-}
+add_action( 'plugins_loaded', 'load_wc_importer_for_danea', 100 );	
