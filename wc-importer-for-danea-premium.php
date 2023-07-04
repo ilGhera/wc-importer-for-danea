6ff345e78cafc0f9474f19fd0135a2c82e8e8fda
ilGhera <andrea@ilghera.com>
2023-07-04 17:26:10 +0200

    Coding standard main file

--- wc-importer-for-danea-premium.php
+++ wc-importer-for-danea-premium.php
@@ -11,6 +11,8 @@
  * Tested up to: 6.2
  * WC tested up to: 7
  * Text Domain: wcifd
+ *
+ * @package wc-importer-for-danea-premium
  */
 
 /*Evito accesso diretto*/
@@ -25,14 +27,14 @@ function load_wc_importer_for_danea_premium() {
 
 	/*Function check */
 	if ( ! function_exists( 'is_plugin_active' ) ) {
-		require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		require_once ABSPATH . '/wp-admin/includes/plugin.php';
 	}
 
 	/*Disattiva il plugin free se presente*/
 	if ( is_plugin_active( 'wc-importer-for-danea/wc-importer-for-danea.php' ) || function_exists( 'load_wc_importer_for_danea' ) ) {
 		deactivate_plugins( 'wc-importer-for-danea/wc-importer-for-danea.php' );
 		remove_action( 'plugins_loaded', 'load_wc_importer_for_danea' );
-		wp_redirect( admin_url( 'plugins.php?plugin_status=all&paged=1&s' ) );
+		wp_safe_redirect( admin_url( 'plugins.php?plugin_status=all&paged=1&s' ) );
 
 	}
 
@@ -48,43 +50,55 @@ function load_wc_importer_for_danea_premium() {
 	load_plugin_textdomain( 'wcifd', false, basename( dirname( __FILE__ ) ) . '/languages' );
 
 	/*Richiamo file necessari*/
-	require_once( WCIFD_DIR . 'libraries/action-scheduler/action-scheduler.php' );
-	require_once( WCIFD_ADMIN . 'wcifd-admin.php' );
-	require_once( WCIFD_ADMIN . 'ilghera-notice/class-ilghera-notice.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-functions.php' );
-	require_once( WCIFD_CLASSES . 'class-wcifd-temporary-data.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-products-images.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-single-product-image.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-orphan-images.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-import-users.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-import-products.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-catalog-update.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-import-single-product.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-product-meta-lookup.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-delete-single-product.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-delete-all-products.php' );
-	require_once( WCIFD_INCLUDES . 'wcifd-import-orders.php' );
+	require_once WCIFD_DIR . 'libraries/action-scheduler/action-scheduler.php';
+	require_once WCIFD_ADMIN . 'wcifd-admin.php';
+	require_once WCIFD_ADMIN . 'ilghera-notice/class-ilghera-notice.php';
+	require_once WCIFD_INCLUDES . 'wcifd-functions.php';
+	require_once WCIFD_CLASSES . 'class-wcifd-temporary-data.php';
+	require_once WCIFD_INCLUDES . 'wcifd-products-images.php';
+	require_once WCIFD_INCLUDES . 'wcifd-single-product-image.php';
+	require_once WCIFD_INCLUDES . 'wcifd-orphan-images.php';
+	require_once WCIFD_INCLUDES . 'wcifd-import-users.php';
+	require_once WCIFD_INCLUDES . 'wcifd-import-products.php';
+	require_once WCIFD_INCLUDES . 'wcifd-catalog-update.php';
+	require_once WCIFD_INCLUDES . 'wcifd-import-single-product.php';
+	require_once WCIFD_INCLUDES . 'wcifd-product-meta-lookup.php';
+	require_once WCIFD_INCLUDES . 'wcifd-delete-single-product.php';
+	require_once WCIFD_INCLUDES . 'wcifd-delete-all-products.php';
+	require_once WCIFD_INCLUDES . 'wcifd-import-orders.php';
 
 }
 add_action( 'after_setup_theme', 'load_wc_importer_for_danea_premium' );
 
 
 /*Richiamo "Update-Checker"*/
-require( plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php' );
+require plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-$wcifdUpdateChecker = PucFactory::buildUpdateChecker(
+$wcifd_update_checker = PucFactory::buildUpdateChecker(
 	'https://www.ilghera.com/wp-update-server-2/?action=get_metadata&slug=wc-importer-for-danea-premium',
 	__FILE__,
 	'wc-importer-for-danea-premium'
 );
 
-$wcifdUpdateChecker->addQueryArgFilter( 'wcifd_secure_update_check' );
-function wcifd_secure_update_check( $queryArgs ) {
+$wcifd_update_checker->addQueryArgFilter( 'wcifd_secure_update_check' );
+
+
+/**
+ * Aggiornamento con chiave di licenza
+ *
+ * @param array $args argomenti funzine di aggiornamento.
+ *
+ * @return array
+ */
+function wcifd_secure_update_check( $args ) {
+
 	$key = base64_encode( get_option( 'wcifd-premium-key' ) );
 
 	if ( $key ) {
-		$queryArgs['premium-key'] = $key;
+		$args['premium-key'] = $key;
 	}
-	return $queryArgs;
+
+	return $args;
+
 }
 
