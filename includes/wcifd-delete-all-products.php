2b0c09efc9c03cdd8626ed78547cbf3a426ba4e6
ilGhera <andrea@ilghera.com>
2023-07-05 19:56:53 +0200

    Coding standard in eliminazione tutti i prodotti

--- includes/wcifd-delete-all-products.php
+++ includes/wcifd-delete-all-products.php
@@ -4,30 +4,36 @@
  *
  * @author ilGhera
  * @package wc-importer-for-danea-premium/includes
- * @since 1.5.2
+ *
+ * @since 1.6.0
  */
 
+/**
+ * Eliminazione prodotti
+ *
+ * @return void
+ */
 function wcifd_delete_all_products() {
 
-    global $wpdb;
+	global $wpdb;
 
-    $queries = array(
-        "DELETE relations.*, taxes.*, terms.*
+	$queries = array(
+		"DELETE relations.*, taxes.*, terms.*
         FROM $wpdb->term_relationships AS relations
         INNER JOIN $wpdb->term_taxonomy AS taxes
         ON relations.term_taxonomy_id=taxes.term_taxonomy_id
         INNER JOIN $wpdb->terms AS terms
         ON taxes.term_id=terms.term_id
         WHERE object_id IN (SELECT ID FROM $wpdb->posts WHERE post_type='product')",
-        "DELETE FROM $wpdb->postmeta WHERE post_id IN (SELECT ID FROM $wpdb->posts WHERE post_type = 'product')",
-        "DELETE FROM $wpdb->posts WHERE post_type = 'product'",
-    );
+		"DELETE FROM $wpdb->postmeta WHERE post_id IN (SELECT ID FROM $wpdb->posts WHERE post_type = 'product')",
+		"DELETE FROM $wpdb->posts WHERE post_type = 'product'",
+	);
 
-    foreach ( $queries as $key => $query ) {
+	foreach ( $queries as $key => $query ) {
 
-        $result = $wpdb->query( $query );
+		$result = $wpdb->query( $query );
 
-    }
+	}
 
 }
 
