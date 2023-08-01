fc258466c38037f8aa4f83f51fff6098f83a5847
ilGhera <andrea@ilghera.com>
2023-08-01 15:13:47 +0200

    Coding standard in clients template

--- admin/wcifd-import-clients-template.php
+++ admin/wcifd-import-clients-template.php
@@ -1,14 +1,21 @@
 <?php
 /**
  * Importazione clienti
+ *
  * @author ilGhera
  * @package wc-importer-for-danea-premium/admin
  * @since 1.6.0
  */
 
 global $wp_roles;
-$roles = $wp_roles->get_names();
-$users_val = ( isset( $_POST['wcifd-users'] ) ) ? sanitize_text_field( $_POST['wcifd-users'] ) : get_option( 'wcifd-clients-role' );
+$roles     = $wp_roles->get_names();
+$users_val = get_option( 'wcifd-clients-role' );
+
+if ( isset( $_POST['wcifd-users'], $_POST['wcifd-clients-nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wcifd-clients-nonce'] ) ), 'wcifd-clients-import' ) ) {
+
+	$users_val = sanitize_text_field( wp_unslash( $_POST['wcifd-users'] ) );
+
+}
 ?>
 
 <!--Form Clienti-->
@@ -16,44 +23,44 @@ $users_val = ( isset( $_POST['wcifd-users'] ) ) ? sanitize_text_field( $_POST['w
 
 	<table class="form-table">
 		<tr>
-			<th scope="row"><?php _e( 'User role', 'wcifd' ); ?></th>
+			<th scope="row"><?php esc_html_e( 'User role', 'wcifd' ); ?></th>
 			<td>
 			<select class="wcifd-users-clients wcifd-select" name="wcifd-users-clients">
 				<?php
 				if ( $users_val ) {
-					echo '<option value=" ' . $users_val . ' " selected="selected"> ' . $users_val . '</option>';
+					echo '<option value=" ' . esc_attr( $users_val ) . ' " selected="selected"> ' . esc_html( $users_val ) . '</option>';
 					foreach ( $roles as $key => $value ) {
-						if ( $key != $users_val ) {
-							echo '<option value=" ' . $key . ' "> ' . $key . '</option>';
+						if ( $key !== $users_val ) {
+							echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
 						}
 					}
 				} else {
 					echo '<option value="Subscriber" selected="selected">Subscriber</option>';
 					foreach ( $roles as $key => $value ) {
-						if ( $key != 'Subscriber' ) {
-							echo '<option value=" ' . $key . ' "> ' . $key . '</option>';
+						if ( 'Subscriber' !== $key ) {
+							echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
 						}
 					}
 				}
 				?>
 			</select>
-			<p class="description"><?php _e( 'Select a Wordpress user role for your clients.', 'wcifd' ); ?></p>
+			<p class="description"><?php esc_html_e( 'Select a WordPress user role for your clients.', 'wcifd' ); ?></p>
 		</tr>
 
 		<?php wp_nonce_field( 'wcifd-clients-import', 'wcifd-clients-nonce' ); ?>
 		<input type="hidden" name="clients-import" value="1">
 
 		<tr>
-			<th scope="row"><?php _e( 'Add clients', 'wcifd' ); ?></th>
+			<th scope="row"><?php esc_html_e( 'Add clients', 'wcifd' ); ?></th>
 			<td>
 				<input type="file" name="clients-list">
-				<p class="description"><?php _e( 'Select your clients list file (.csv)', 'wcifd' ); ?></p>
+				<p class="description"><?php esc_html_e( 'Select your clients list file (.csv)', 'wcifd' ); ?></p>
 			</td>
 		</tr>
 
 	</table>
 
-	<input type="submit" class="button-primary" value="<?php _e( 'Import Clients', 'wcifd' ); ?>">
+	<input type="submit" class="button-primary" value="<?php esc_html_e( 'Import Clients', 'wcifd' ); ?>">
 
 </form>
 
