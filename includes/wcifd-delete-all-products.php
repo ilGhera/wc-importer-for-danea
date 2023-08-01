89fb6438b3c69a684f0e02c0376e75479ab6ac75
ilGhera <andrea@ilghera.com>
2023-08-01 15:55:44 +0200

    Coding standard in delete all products

--- includes/wcifd-delete-all-products.php
+++ includes/wcifd-delete-all-products.php
@@ -7,27 +7,32 @@
  * @since 1.2.1
  */
 
+/**
+ * WCIFD delete all products
+ *
+ * @retur void
+ */
 function wcifd_delete_all_products() {
 
-    global $wpdb;
+	global $wpdb;
 
-    $queries = array(
-        "DELETE relations.*, taxes.*, terms.*
+	$queries = array(
+		"DELETE relations.*, taxes.*, terms.*
         FROM $wpdb->term_relationships AS relations
         INNER JOIN $wpdb->term_taxonomy AS taxes
         ON relations.term_taxonomy_id=taxes.term_taxonomy_id
         INNER JOIN $wpdb->terms AS terms
         ON taxes.term_id=terms.term_id
         WHERE object_id IN (SELECT ID FROM $wpdb->posts WHERE post_type='product')",
-        "DELETE FROM $wpdb->postmeta WHERE post_id IN (SELECT ID FROM $wpdb->posts WHERE post_type = 'product')",
-        "DELETE FROM $wpdb->posts WHERE post_type = 'product'",
-    );
+		"DELETE FROM $wpdb->postmeta WHERE post_id IN (SELECT ID FROM $wpdb->posts WHERE post_type = 'product')",
+		"DELETE FROM $wpdb->posts WHERE post_type = 'product'",
+	);
 
-    foreach ( $queries as $key => $query ) {
+	foreach ( $queries as $key => $query ) {
 
-        $result = $wpdb->query( $query );
+		$result = $wpdb->query( $query );
 
-    }
+	}
 
 }
 
