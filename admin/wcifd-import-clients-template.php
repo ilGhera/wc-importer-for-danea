6fc5fc845d9b8d622095bf4f0896549662683bb8
ilGhera <andrea@ilghera.com>
2024-06-05 13:19:10 +0200

    Coding standard in clients template

--- admin/wcifd-import-clients-template.php
+++ admin/wcifd-import-clients-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Importazione clienti
+ * Clients template
  *
  * @author ilGhera
  * @package wc-importer-for-danea-premium/admin
@@ -19,52 +19,51 @@ if ( isset( $_POST['wcifd-users'], $_POST['wcifd-clients-nonce'] ) && wp_verify_
 }
 ?>
 
-<!--Form Clienti-->
 <div id="wcifd-clients" class="wcifd-admin">
-    <form name="wcifd-clients-import" id="wcifd-clients-import" class="wcifd-form"  method="post" enctype="multipart/form-data" action="">
+	<form name="wcifd-clients-import" id="wcifd-clients-import" class="wcifd-form"  method="post" enctype="multipart/form-data" action="">
 
-        <table class="form-table">
-            <tr>
-                <th scope="row"><?php esc_html_e( 'User role', 'wc-importer-for-danea' ); ?></th>
-                <td>
-                <select class="wcifd-users-clients wcifd-select" name="wcifd-users-clients">
-                    <?php
-                    if ( $users_val ) {
-                        echo '<option value=" ' . esc_attr( $users_val ) . ' " selected="selected"> ' . esc_html( $users_val ) . '</option>';
-                        foreach ( $roles as $key => $value ) {
-                            if ( $key !== $users_val ) {
-                                echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
-                            }
-                        }
-                    } else {
-                        echo '<option value="Subscriber" selected="selected">Subscriber</option>';
-                        foreach ( $roles as $key => $value ) {
-                            if ( 'Subscriber' !== $key ) {
-                                echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
-                            }
-                        }
-                    }
-                    ?>
-                </select>
-                <p class="description"><?php esc_html_e( 'Select a WordPress user role for your clients.', 'wc-importer-for-danea' ); ?></p>
-            </tr>
+		<table class="form-table">
+			<tr>
+				<th scope="row"><?php esc_html_e( 'User role', 'wc-importer-for-danea' ); ?></th>
+				<td>
+				<select class="wcifd-users-clients wcifd-select" name="wcifd-users-clients">
+					<?php
+					if ( $users_val ) {
+						echo '<option value=" ' . esc_attr( $users_val ) . ' " selected="selected"> ' . esc_html( $users_val ) . '</option>';
+						foreach ( $roles as $key => $value ) {
+							if ( $key !== $users_val ) {
+								echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
+							}
+						}
+					} else {
+						echo '<option value="Subscriber" selected="selected">Subscriber</option>';
+						foreach ( $roles as $key => $value ) {
+							if ( 'Subscriber' !== $key ) {
+								echo '<option value=" ' . esc_attr( $key ) . ' "> ' . esc_html( $key ) . '</option>';
+							}
+						}
+					}
+					?>
+				</select>
+				<p class="description"><?php esc_html_e( 'Select a WordPress user role for your clients.', 'wc-importer-for-danea' ); ?></p>
+			</tr>
 
-            <?php wp_nonce_field( 'wcifd-clients-import', 'wcifd-clients-nonce' ); ?>
-            <input type="hidden" name="clients-import" value="1">
+			<?php wp_nonce_field( 'wcifd-clients-import', 'wcifd-clients-nonce' ); ?>
+			<input type="hidden" name="clients-import" value="1">
 
-            <tr>
-                <th scope="row"><?php esc_html_e( 'Add clients', 'wc-importer-for-danea' ); ?></th>
-                <td>
-                    <input type="file" name="clients-list">
-                    <p class="description"><?php esc_html_e( 'Select your clients list file (.csv)', 'wc-importer-for-danea' ); ?></p>
-                </td>
-            </tr>
+			<tr>
+				<th scope="row"><?php esc_html_e( 'Add clients', 'wc-importer-for-danea' ); ?></th>
+				<td>
+					<input type="file" name="clients-list">
+					<p class="description"><?php esc_html_e( 'Select your clients list file (.csv)', 'wc-importer-for-danea' ); ?></p>
+				</td>
+			</tr>
 
-        </table>
+		</table>
 
-        <input type="submit" class="button-primary" value="<?php esc_html_e( 'Import Clients', 'wc-importer-for-danea' ); ?>">
+		<input type="submit" class="button-primary" value="<?php esc_html_e( 'Import Clients', 'wc-importer-for-danea' ); ?>">
 
-    </form>
+	</form>
 </div>
 
 <?php wcifd_users( 'clients' ); ?>
