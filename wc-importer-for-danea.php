78dc3f316dd8a7031e23f70d8f06a509acb6dbd7
ilGhera <andrea@ilghera.com>
2025-04-23 11:53:10 +0200

    Cambio ordine requires

--- wc-importer-for-danea.php
+++ wc-importer-for-danea.php
@@ -52,18 +52,18 @@ function load_wc_importer_for_danea_premium() {
 	require_once WCIFD_ADMIN . 'class-wcifd-admin.php';
 	require_once WCIFD_ADMIN . 'ilghera-notice/class-ilghera-notice.php';
 	require_once WCIFD_CLASSES . 'class-wcifd-functions.php';
+	require_once WCIFD_INCLUDES . 'wcifd-delete-all-products.php';
+	require_once WCIFD_CLASSES . 'class-wcifd-temporary-data.php';
+	require_once WCIFD_INCLUDES . 'wcifd-catalog-update.php';
 	require_once WCIFD_CLASSES . 'class-wcifd-import-products.php';
 	require_once WCIFD_CLASSES . 'class-wcifd-import-single-product.php';
-	require_once WCIFD_CLASSES . 'class-wcifd-temporary-data.php';
 	require_once WCIFD_CLASSES . 'class-wcifd-product-meta-lookup.php';
 	require_once WCIFD_CLASSES . 'class-wcifd-progress-bar.php';
 	require_once WCIFD_INCLUDES . 'wcifd-products-images.php';
 	require_once WCIFD_INCLUDES . 'wcifd-single-product-image.php';
 	require_once WCIFD_INCLUDES . 'wcifd-orphan-images.php';
 	require_once WCIFD_INCLUDES . 'wcifd-import-users.php';
-	require_once WCIFD_INCLUDES . 'wcifd-catalog-update.php';
 	require_once WCIFD_INCLUDES . 'wcifd-delete-single-product.php';
-	require_once WCIFD_INCLUDES . 'wcifd-delete-all-products.php';
 	require_once WCIFD_INCLUDES . 'wcifd-import-orders.php';
 
 }
