65acdceb4e14c42287e516781c70540199850c5a
ilGhera <andrea@ilghera.com>
2025-05-07 22:13:17 +0200

    HPOS compatibility

--- wc-importer-for-danea.php
+++ wc-importer-for-danea.php
@@ -78,6 +78,18 @@ function wcifd_init() {
 add_action( 'init', 'wcifd_init', PHP_INT_MIN );
 
 /**
+ * HPOS compatibility
+ */
+add_action(
+	'before_woocommerce_init',
+	function() {
+		if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+		}
+	}
+);
+
+/**
  * Plugin Update Checker
  *
  * @return void
