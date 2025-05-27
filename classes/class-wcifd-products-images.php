1957235e3481f650bcf74fd12f74c878795b3ba9
ilGhera <andrea@ilghera.com>
2025-05-27 17:14:58 +0200

    Riorganizzazione codice classe products images Coding standard classe products images

--- classes/class-wcifd-products-images.php
+++ classes/class-wcifd-products-images.php
@@ -35,75 +35,71 @@ class WCIFD_Products_Images {
 	public function handle_image_upload() {
 
 		if ( ! function_exists( 'wp_handle_upload' ) ) {
+
 			require_once ABSPATH . 'wp-admin/includes/file.php';
 		}
 
-        if ( ! function_exists( 'wp_generate_attachment_metadata' ) ) {
-            require_once ABSPATH . 'wp-admin/includes/image.php';
-        }
+		if ( ! function_exists( 'wp_generate_attachment_metadata' ) ) {
+
+			require_once ABSPATH . 'wp-admin/includes/image.php';
+		}
 
 		$file = isset( $_FILES['file'] ) ? $_FILES['file'] : null;
 
-        if ( ! $file || ! is_array( $file ) || empty( $file['tmp_name'] ) ) {
-            error_log( 'WCIFD ERROR | Immagine: nessun file ricevuto.' );
-            echo 'OK'; // O un messaggio di errore più specifico per il frontend
-            return;
-        }
+		if ( ! $file || ! is_array( $file ) || empty( $file['tmp_name'] ) ) {
 
-        // Start
-        $original_file_name = $file['name'];
+			echo 'OK';
+			return;
+		}
 
-        // Ottieni la directory di upload
-        $upload_dir = wp_upload_dir();
-        $upload_path = $upload_dir['path'];
+		/* The original filename */
+		$original_file_name = $file['name'];
 
-        // Determina un nome di file univoco, basato sul nome originale
-        // Questo è il cuore della soluzione per prevenire il rincorsa di nomi
-        $unique_file_name = wp_unique_filename( $upload_path, $original_file_name );
+		/* Get the upload directory */
+		$upload_dir  = wp_upload_dir();
+		$upload_path = $upload_dir['path'];
 
-        // Crea un nuovo array $_FILES per wp_handle_upload con il nome univoco desiderato
-        // Questo è fondamentale per "forzare" il nome del file se wp_handle_upload non lo fa da solo.
-        $uploaded_file_data = array(
-            'name'     => $unique_file_name, // Il nome desiderato
-            'type'     => $file['type'],
-            'tmp_name' => $file['tmp_name'],
-            'error'    => $file['error'],
-            'size'     => $file['size'],
-        );
+		/* Create a unique filename based on the original file name */
+		$unique_file_name = wp_unique_filename( $upload_path, $original_file_name );
 
-        /* Delete duplicates - Fai attenzione con questa logica */
-        // Se vuoi eliminare le immagini *prima* di caricare la nuova,
-        // assicurati che 'name' nel get_posts corrisponda allo SLUG effettivo.
-        // Oppure, meglio ancora, usa un meta per la deduplicazione.
-        $this->delete_duplicates_by_original_name( $original_file_name ); // Vedi sotto per la modifica
-
-        // End
+		/* The uploaded file data */
+		$uploaded_file_data = array(
+			'name'     => $unique_file_name,
+			'type'     => $file['type'],
+			'tmp_name' => $file['tmp_name'],
+			'error'    => $file['error'],
+			'size'     => $file['size'],
+		);
 
 		/* Delete duplicates */
-		/* $this->delete_duplicates( $file ); */
+		$this->delete_duplicates( $original_file_name );
 
-		/* Load image in WP Media */
+		/* Upload the image in WP Media */
 		$wp_image = wp_handle_upload( $uploaded_file_data, array( 'test_form' => false ) );
 
 		if ( isset( $wp_image['error'] ) ) {
+
 			error_log( 'WCIFD ERROR | Immagine: ' . $file['name'] . ' | ' . print_r( $wp_image['error'], true ) );
 			echo 'OK';
 			return;
 		} elseif ( ! $wp_image ) {
+
 			error_log( 'WCIFD ERROR | Immagine: ' . $file['name'] . ' |  Errore di ricezione' );
 			echo 'OK';
 			return;
 		}
 
-        $filetype = wp_check_filetype( basename( $wp_image['file'] ), null ); // Usa il nome file effettivamente caricato da wp_handle_upload
+		/* Get the file type */
+		$filetype = wp_check_filetype( basename( $wp_image['file'] ), null );
 
-         $attachment = array(
-            'guid'           => $wp_image['url'],
-            'post_mime_type' => $filetype['type'],
-            'post_title'     => sanitize_title( pathinfo( $unique_file_name, PATHINFO_FILENAME ) ), // Titolo basato sul nome univoco
-            'post_content'   => '',
-            'post_status'    => 'inherit',
-        );
+		/* The attachment data */
+		$attachment = array(
+			'guid'           => $wp_image['url'],
+			'post_mime_type' => $filetype['type'],
+			'post_title'     => sanitize_title( pathinfo( $unique_file_name, PATHINFO_FILENAME ) ),
+			'post_content'   => '',
+			'post_status'    => 'inherit',
+		);
 
 		/* Add attachment */
 		$attach_id = wp_insert_attachment( $attachment, $wp_image['file'] );
@@ -112,75 +108,101 @@ class WCIFD_Products_Images {
 		$attach_data = wp_generate_attachment_metadata( $attach_id, $wp_image['file'] );
 		wp_update_attachment_metadata( $attach_id, $attach_data );
 
-         // Fondamentale: Memorizza il nome originale del file come meta.
-        // Questo sarà il tuo riferimento stabile per abbinare ai prodotti.
-        update_post_meta( $attach_id, '_wcifd_original_filename', $original_file_name );
+		/* Add post meta to the attachment */
+		update_post_meta( $attach_id, '_wcifd_original_filename', $original_file_name );
 
 		echo 'OK';
 	}
 
-    /**
-     * Delete duplicates by original filename stored in post meta.
-     *
-     * @param string $original_filename The original filename from the Danea system.
-     *
-     * @return void
-     */
-    public function delete_duplicates_by_original_name( $original_filename ) {
+	/**
+	 * Delete duplicates by original filename stored in post meta.
+	 *
+	 * @param string $original_filename The original filename from the Danea system.
+	 *
+	 * @return void
+	 */
+	public function delete_duplicates( $original_filename ) {
 
-        $image_ids_to_delete = array();
+		$image_ids_to_delete = array();
 
-        // 1. Cerca usando il nuovo post meta (il metodo preferito)
-        $args_new_meta = array(
-            'post_type'      => 'attachment',
-            'post_status'    => 'inherit',
-            'meta_query'     => array(
-                array(
-                    'key'     => '_wcifd_original_filename',
-                    'value'   => $original_filename,
-                    'compare' => '=',
-                ),
-            ),
-            'fields'         => 'ids',
-            'posts_per_page' => -1,
-            'no_found_rows'  => true,
-        );
-        $image_ids_new_meta = get_posts( $args_new_meta );
+		$image_ids_by_meta = $this->get_image_ids_by_meta( $original_filename );
 
-        if ( ! empty( $image_ids_new_meta ) ) {
-            // Trovati duplicati con il nuovo meta: elimina questi
-            $image_ids_to_delete = $image_ids_new_meta;
-            error_log( 'POSTMETA - IMAGE IDS TO DELETE: ' . print_r( $image_ids_to_delete, true ) );
-        } else {
-            // 2. Se nessun duplicato trovato con il nuovo meta, prova con il vecchio metodo (post_name)
-            // Questo cattura le immagini importate prima dell'introduzione del meta
-            $sanitized_slug = sanitize_title( pathinfo( $original_filename, PATHINFO_FILENAME ) );
-            error_log( 'NOME IMMAGINE - SANITIZED SLUG: ' . $sanitized_slug );
+		if ( ! empty( $image_ids_by_meta ) ) {
 
-            $args_old_slug = array(
-                'post_type'      => 'attachment',
-                'post_status'    => 'inherit',
-                'name'           => $sanitized_slug, // Ricerca esatta sullo slug del post
-                'fields'         => 'ids',
-                'posts_per_page' => -1,
-                'no_found_rows'  => true,
-            );
-            $image_ids_old_slug = get_posts( $args_old_slug );
+			$image_ids_to_delete = $image_ids_by_meta;
+			error_log( 'POSTMETA - IMAGE IDS TO DELETE: ' . print_r( $image_ids_to_delete, true ) );
 
-            if ( ! empty( $image_ids_old_slug ) ) {
-                // Trovati duplicati con il vecchio slug: elimina questi
-                $image_ids_to_delete = $image_ids_old_slug;
-                error_log( 'NOME IMMAGINE - IMAGE IDS TO DELETE: ' . print_r( $image_ids_to_delete, true ) );
-            }
-        }
+		} else {
 
-        // Processa l'eliminazione
-        if ( ! empty( $image_ids_to_delete ) ) {
-            foreach ( $image_ids_to_delete as $id ) {
-                wp_delete_post( $id, true ); // true per forzare la cancellazione dal database
-                error_log( 'WCIFD INFO | Eliminato duplicato (criterio misto) per ' . $original_filename . ' (ID: ' . $id . ')' );
-            }
-        }
-    }
+			$image_ids_by_name = $this->get_image_ids_by_name( $original_filename );
+
+			if ( ! empty( $image_ids_old_slug ) ) {
+
+				$image_ids_to_delete = $image_ids_old_slug;
+				error_log( 'NOME IMMAGINE - IMAGE IDS TO DELETE: ' . print_r( $image_ids_to_delete, true ) );
+			}
+		}
+
+		/* Deleting process */
+		if ( ! empty( $image_ids_to_delete ) ) {
+
+			foreach ( $image_ids_to_delete as $id ) {
+
+				wp_delete_post( $id, true );
+				error_log( 'WCIFD INFO | Eliminato duplicato per ' . $original_filename . ' (ID: ' . $id . ')' );
+			}
+		}
+	}
+
+	/**
+	 * Get images by original filename stored in post meta.
+	 *
+	 * @param string $original_filename The original filename from the Danea system.
+	 *
+	 * @return array An array of attachment IDs.
+	 */
+	public function get_image_ids_by_meta( $original_filename ) {
+
+		$args = array(
+			'post_type'      => 'attachment',
+			'post_status'    => 'inherit',
+			'meta_query'     => array(
+				array(
+					'key'     => '_wcifd_original_filename',
+					'value'   => $original_filename,
+					'compare' => '=',
+				),
+			),
+			'fields'         => 'ids',
+			'posts_per_page' => -1,
+			'no_found_rows'  => true,
+		);
+
+		return get_posts( $args );
+	}
+
+	/**
+	 * Get images by original filename.
+	 *
+	 * @param string $original_filename The original filename from the Danea system.
+	 *
+	 * @return array An array of attachment IDs.
+	 */
+	public function get_image_ids_by_name( $original_filename ) {
+
+		$sanitized_slug = sanitize_title( pathinfo( $original_filename, PATHINFO_FILENAME ) );
+		error_log( 'NOME IMMAGINE - SANITIZED SLUG: ' . $sanitized_slug );
+
+		$args = array(
+			'post_type'      => 'attachment',
+			'post_status'    => 'inherit',
+			'name'           => $sanitized_slug,
+			'fields'         => 'ids',
+			'posts_per_page' => -1,
+			'no_found_rows'  => true,
+		);
+
+		return get_posts( $args );
+	}
 }
 
